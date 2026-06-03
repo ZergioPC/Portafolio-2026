149a067556aa03a0e034031b77e1a27a22eecf48
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-06-03 00:01:38 -0500

    arreglos a la seccion de tarjetas de mainprojects

--- src/components/section_dev/MainProjectsContainer/script.ts
+++ src/components/section_dev/MainProjectsContainer/script.ts
@@ -1,6 +1,6 @@
 type CardState = "open" | "close" | "default"
 
-const cardsControler: CardState[] = ["default", "default", "default"]
+const cardsControler: CardState[] = ["default", "default"]
 
 const $cards = document.querySelectorAll<HTMLElement>("[data-dev-card]")
 const $btnReset = document.getElementById("btnReset")
@@ -10,9 +10,11 @@ function controllerAllClose():void {
   for (let i = 0; i < cardsControler.length; i++){
     cardsControler[i] = "default"
   }
+  if ($btnReset) $btnReset.style.opacity = "0%"
 }
 
 function controlerUpdate(index:number):void {
+  if ($btnReset) $btnReset.style.opacity = "100%"
   for (let i = 0; i < cardsControler.length; i++) {
     if (i === index) {
       cardsControler[i] = "open"
