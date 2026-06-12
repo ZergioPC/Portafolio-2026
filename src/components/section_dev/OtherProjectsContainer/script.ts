ee634bb00cfcc1bd26d3b1ab1acb59d7644bd121
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-06-12 11:46:43 -0500

    Cambios en el contenido a mostrar

--- src/components/section_dev/OtherProjectsContainer/script.ts
+++ src/components/section_dev/OtherProjectsContainer/script.ts
@@ -1,11 +1,5 @@
 // Grafico de Lenguajes
 
-const aux_data = {
-  "JavaScript": 44694,
-  "CSS": 16512,
-  "HTML": 575
-}
-
 function setLanguageStyle(lang:string): string{
   switch(lang){
     case "Astro":
@@ -29,10 +23,10 @@ const $langsContainers = document.querySelectorAll<HTMLUListElement>(".lang-cont
 
 $langsContainers.forEach(async ul =>{
   const path = ul.getAttribute("data-langs")
-  // const res = await fetch(path ?? "")
-  // if (!res.ok) return
-  // const lenguajes:Record<string, number> = await res.json()
-  const lenguajes:Record<string, number> = aux_data
+  const res = await fetch(path ?? "")
+  if (!res.ok) return
+  const lenguajes:Record<string, number> = await res.json()
+  
   const lenguajes_sorted = Object.entries(lenguajes).sort((a, b) => b[1] - a[1])
 
   lenguajes_sorted.slice(0,3).forEach(lang => {
